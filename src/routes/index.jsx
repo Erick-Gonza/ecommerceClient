b162dbb4fbb93e6918801f29252f3eb80dc5c488
Odalys Guerrero <odalysguerrerol@gmail.com>
2023-02-26 00:19:28 -0600

    cartView and cartCart

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,7 +3,7 @@ import App from '../App'
 import Home from '../views/Home'
 import SignIn from '../views/SignIn'
 import SignUp from '../views/SignUp'
-
+import SortFilter from '../views/SortFilter'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -20,6 +20,10 @@ const router = createBrowserRouter([
       {
         path: '/signup',
         element: <SignUp />,
+      },
+      {
+        path: '/filter',
+        element: <SortFilter/> ,
       },
     ],
   },
