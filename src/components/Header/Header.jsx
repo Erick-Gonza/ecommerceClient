c83fc3de8b214e9bf9052865f366aa9b0a2f2577
erickGzz <eagm080897@gmail.com>
2023-02-27 09:59:43 -0600

    Add formatting style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import { Nav } from "./Navbar/Nav.jsx";
+import { Link } from 'react-router-dom'
+import { Nav } from './Navbar/Nav.jsx'
 
 const Header = () => {
   return (
@@ -7,32 +7,32 @@ const Header = () => {
       <Nav />
       <section className="flex w-4/5 justify-around px-3 py-2 border-slate-600">
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          NEW{" "}
-        </Link>{" "}
+          NEW
+        </Link>
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          WEAR{" "}
-        </Link>{" "}
+          WEAR
+        </Link>
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          ACCESORIES{" "}
-        </Link>{" "}
+          ACCESORIES
+        </Link>
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          BAGS{" "}
-        </Link>{" "}
+          BAGS
+        </Link>
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          TOYS{" "}
-        </Link>{" "}
+          TOYS
+        </Link>
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          SHOES{" "}
-        </Link>{" "}
+          SHOES
+        </Link>
         <Link
           to="/"
           className="font-bold text-red-400 cursor-pointer hover:scale-105"
         >
-          SALES{" "}
-        </Link>{" "}
-      </section>{" "}
+          SALES
+        </Link>
+      </section>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
