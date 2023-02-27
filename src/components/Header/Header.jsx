97f65b4b2824fa124694490c735eb46ccb3e8c28
Norma <nidaliaamtz@hotmail.com>
2023-02-26 23:06:02 -0600

    Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,38 @@
-import { Link } from 'react-router-dom'
-import { Nav } from './Navbar/Nav'
+import { Link } from "react-router-dom";
+import { Nav } from "./Navbar/Nav";
 
-const Footer = () => {
+const Header = () => {
   return (
     <header className="flex flex-col px-3 py-2 justify-center items-center w-full h-auto">
       <Nav />
-
       <section className="flex w-4/5 justify-around px-3 py-2">
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          New
-        </Link>
+          New{" "}
+        </Link>{" "}
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          Wear
-        </Link>
+          Wear{" "}
+        </Link>{" "}
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          Accessories
-        </Link>
+          Accessories{" "}
+        </Link>{" "}
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          Bags
-        </Link>
+          Bags{" "}
+        </Link>{" "}
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          Toys
-        </Link>
+          Toys{" "}
+        </Link>{" "}
         <Link to="/" className="font-semibold cursor-pointer hover:scale-105">
-          Shoes
-        </Link>
+          Shoes{" "}
+        </Link>{" "}
         <Link
           to="/"
           className="font-bold text-red-400 cursor-pointer hover:scale-105"
         >
-          Sales
-        </Link>
-      </section>
+          Sales{" "}
+        </Link>{" "}
+      </section>{" "}
     </header>
-  )
-}
+  );
+};
 
-export default Footer
+export default Header;
--- src/components/Header/Navbar/Nav.jsx
+++ src/components/Header/Navbar/Nav.jsx
@@ -1,45 +1,99 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
   return (
-    <nav className="flex w-full justify-around py-4 px-3">
-      <section className="w-full">
-        <h2>Logo</h2>
-      </section>
+    <section className="w-full bg-primary">
+      <nav className="w-full px-4 py-3 flex text-white">
+        <section className="w-auto">
+          <h2>Logo</h2>
+        </section>
 
-      <section className="relative border border-gray-200 rounded-md">
-        <div className="absolute top-5 left-3">
-          <img
-            src="https://img.icons8.com/ios/256/search.png"
-            alt="search"
-            className="object-contain h-4 w-4"
+        <section className="hidden md:block relative rounded-md mx-3">
+          <div className="absolute top-5 left-1">
+            <img
+              src="https://img.icons8.com/ios/256/search.png"
+              alt="search"
+              className="object-contain h-4 w-4"
+            />
+          </div>
+          <input
+            type="text"
+            className="h-14 md:w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+            placeholder="Search anything..."
           />
-        </div>
-        <input
-          type="text"
-          className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-          placeholder="Search anything..."
-        />
-        <div className="absolute top-2 right-2">
-          <button className="h-10 w-20 rounded-lg bg-gray-200">Search</button>
-        </div>
-      </section>
+          <div className="absolute top-2 right-2">
+            <button className="h-10 w-20 rounded-lg bg-gray-400">Search</button>
+          </div>
+        </section>
 
-      <section className="flex justify-center items-center gap-8 w-full">
-        <Link to="/signin">Login</Link>
-        <Link to="/signup">Register</Link>
-        <Link to="/favorites">Fav</Link>
-        <Link to="/cart">Cart</Link>
-        <button>
-          {/* add icon for theme */}
-          <img
-            src="https://img.icons8.com/ios-glyphs/256/bright-moon.png"
-            alt="dark mode"
-            className="object-contain h-6 w-6"
-          />
-        </button>
-      </section>
-    </nav>
-  )
-}
+        <section className="hidden md:flex justify-center items-center gap-8 w-full text-sm text-white">
+          <Link className="flex flex-row" to="/home">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+              <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+            </svg>
+            HOME
+          </Link>
+          <Link className="flex flex-row" to="/favorites">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+            </svg>
+            FAV
+          </Link>
+          <Link className="flex flex-row" to="/cart">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+            </svg>
+            CART
+          </Link>
+          <Link className="flex flex-row" to="/signin">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+            LOGIN / SIGN UP
+          </Link>
+          <button>
+            {/* add icon for theme */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </section>
+      </nav>
+    </section>
+  );
+};
