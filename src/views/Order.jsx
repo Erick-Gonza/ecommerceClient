8b7489f9c4df8f1530079bcbda7d38eff9f53724
erickGzz <eagm080897@gmail.com>
2023-03-02 17:55:05 -0600

    Update fromating style

--- src/views/Order.jsx
+++ src/views/Order.jsx
@@ -1,93 +1,97 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Order = () => {
-    const dataProducts = [
-        {
-            id: "id1",
-            name: "Product Name 1",
-            price: "10.00",
-            description: "Product description 1",
-            quantity: 1,
-            img: "https://picsum.photos/1920/800"
-        },
-        {
-            id: "id2",
-            name: "Product Name 2",
-            price: "20.00",
-            description: "Product description 2",
-            quantity: 1,
-            img: "https://picsum.photos/1920/800"
-        },
-        {
-            id: "id3",
-            name: "Product Name 3",
-            price: "30.00",
-            description: "Product description 3",
-            quantity: 1,
-            img: "https://picsum.photos/1920/800"
-        }
-    ];
+  const dataProducts = [
+    {
+      id: 'id1',
+      name: 'Product Name 1',
+      price: '10.00',
+      description: 'Product description 1',
+      quantity: 1,
+      img: 'https://picsum.photos/1920/800',
+    },
+    {
+      id: 'id2',
+      name: 'Product Name 2',
+      price: '20.00',
+      description: 'Product description 2',
+      quantity: 1,
+      img: 'https://picsum.photos/1920/800',
+    },
+    {
+      id: 'id3',
+      name: 'Product Name 3',
+      price: '30.00',
+      description: 'Product description 3',
+      quantity: 1,
+      img: 'https://picsum.photos/1920/800',
+    },
+  ]
 
-    const totalQuantity = dataProducts.reduce(
-        (total, product) => total + product.quantity,
-        0
-    );
-    const totalPrice = dataProducts.reduce(
-        (total, product) => total + parseFloat(product.price),
-        0
-    );
+  const totalQuantity = dataProducts.reduce(
+    (total, product) => total + product.quantity,
+    0
+  )
+  const totalPrice = dataProducts.reduce(
+    (total, product) => total + parseFloat(product.price),
+    0
+  )
 
+  return (
+    <section className="w-full flex flex-col justify-center items-center px-4 py-2 gap-y-4 drop-shadow-lg">
+      <section className="w-full">
+        <h2 className="text-center text-2xl font-bold">Order Summary</h2>
+      </section>
 
-
-    return (
-        <section className="w-full flex flex-col justify-center items-center px-4 py-2 gap-y-4 drop-shadow-lg">
-            <section className="w-full">
-                <h2 className="text-center text-2xl font-bold">Order Summary</h2>
-            </section>
-
-            <section className="flex w-full flex-col justify-center items-center md:flex-row mt-2 relative ">
-                <section className="flex md:w-1/2 gap-2 px-1 py-5">
-                    <section className="flex flex-col w-full gap-y-12 drop-shadow-lg items-center">
-                        {dataProducts.map((product) => (
-                            <section key={product.id} className="flex md:flex-row flex-col items-center" >
-                                <section className="md:w-full">
-                                    <h2 className="w-full p-1">Name: {product.name}</h2>
-                                    <h2 className="w-full p-1">Description: {product.description}</h2>
-                                    <h2 className="w-full p-1">Price: {product.price}</h2>
-                                    <h2 className="w-full p-1">Quantity: {product.quantity} </h2>
-                                </section>
-                                <section className="w-full items-end"> 
-                                    <img
-                                    className="object-cover aspect-square drop-shadow-lg rounded h-64 md:h-48"
-                                    src={product.img}
-                                    alt={product.name}
-                                    />
-                                </section>
-                            </section>
-                        ))}
-                    </section>
+      <section className="flex w-full flex-col justify-center items-center md:flex-row mt-2 relative ">
+        <section className="flex md:w-1/2 gap-2 px-1 py-5">
+          <section className="flex flex-col w-full gap-y-12 drop-shadow-lg items-center">
+            {dataProducts.map((product) => (
+              <section
+                key={product.id}
+                className="flex md:flex-row flex-col items-center"
+              >
+                <section className="md:w-full">
+                  <h2 className="w-full p-1">Name: {product.name}</h2>
+                  <h2 className="w-full p-1">
+                    Description: {product.description}
+                  </h2>
+                  <h2 className="w-full p-1">Price: {product.price}</h2>
+                  <h2 className="w-full p-1">Quantity: {product.quantity} </h2>
                 </section>
-            </section>
-
-            <section className="flex flex-row font-bold justify-center">
-                <section className="w-full gap-y-4">
-                    <h2>Total Quantity:</h2>
-                    <h2>Total Price: </h2>
+                <section className="w-full items-end">
+                  <img
+                    className="object-cover aspect-square drop-shadow-lg rounded h-64 md:h-48"
+                    src={product.img}
+                    alt={product.name}
+                  />
                 </section>
-                <section className="w-full gap-y-4">
-                            <h2>{totalQuantity}</h2>
-                            <h2>{totalPrice.toFixed(2)}</h2>
-                </section>
-                <Link to={"/"}
-                    className="flex w-40  hover:scale-105 drop-shadow-md justify-center bg-primary text-white font-bold px-3 py-2 rounded-lg mt-4 mb-4 cursor-pointer h-10"
-                > <p>Back</p>
-                </Link>
-            </section>
-
+              </section>
+            ))}
+          </section>
         </section>
-    );
-};
+      </section>
 
+      <section className="flex flex-row font-bold justify-center">
+        <section className="w-full gap-y-4">
+          <h2>Total Quantity:</h2>
+          <h2>Total Price: </h2>
+        </section>
+        <section className="w-full gap-y-4">
+          <h2>{totalQuantity}</h2>
+          <h2>{totalPrice.toFixed(2)}</h2>
+        </section>
+        <Link
+          to={'/'}
+          className="flex w-40  hover:scale-105 drop-shadow-md justify-center bg-primary text-white font-bold px-3 py-2 rounded-lg mt-4 mb-4 cursor-pointer h-10"
+        >
+          {' '}
+          <p>Back</p>
+        </Link>
+      </section>
+    </section>
+  )
+}
 
 export default Order
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,62 +1,82 @@
 import { useState } from 'react'
 
 const dataUser = {
-    name: 'testName',
-    lastName: 'testLastName',
-    userName: 'testUserName',
-    email: 'test@test.com',
-    address: 'test address',
+  name: 'testName',
+  lastName: 'testLastName',
+  userName: 'testUserName',
+  email: 'test@test.com',
+  address: 'test address',
 }
 
 const Profile = () => {
-    const [isEditMode, setIsEditMode] = useState(false)
+  const [isEditMode, setIsEditMode] = useState(false)
 
-    const toggleEditMode = () => {
-        setIsEditMode(!isEditMode)
-    }
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode)
+  }
 
-    return (
-        <section className="w-full flex flex-col justify-center items-center px-4 py-2 ">
-            <section className="w-full">
-                <h2 className="text-center text-2xl font-bold">My Account</h2>
+  return (
+    <section className="w-full flex flex-col justify-center items-center px-4 py-2 ">
+      <section className="w-full">
+        <h2 className="text-center text-2xl font-bold">My Account</h2>
+      </section>
+
+      <section className="flex w-full flex-col justify-center items-center md:items-end md:flex-row mt-2 relative ">
+        <section className="flex md:w-1/2 gap-4 px-1 py-5">
+          <section className="flex flex-col gap-y-2 w-1/2 text-right ">
+            <h2 className="p-1">First Name</h2>
+            <h2 className="p-1">Last Name</h2>
+            <h2 className="p-1">UserName</h2>
+            <h2 className="p-1">Email</h2>
+            <h2 className="p-1">Address</h2>
+          </section>
+          {!isEditMode ? (
+            <section className="flex flex-col md:w-1/2 gap-y-2">
+              <h2 className="p-1">{dataUser?.name}</h2>
+              <h2 className="p-1">{dataUser?.lastName}</h2>
+              <h2 className="p-1">{dataUser?.userName}</h2>
+              <h2 className="p-1">{dataUser?.email}</h2>
+              <h2 className="p-1">{dataUser?.address}</h2>
             </section>
-
-            <section className="flex w-full flex-col justify-center items-center md:items-end md:flex-row mt-2 relative ">
-                <section className="flex md:w-1/2 gap-4 px-1 py-5">
-                    <section className="flex flex-col gap-y-2 w-1/2 text-right ">
-                        <h2 className="p-1">First Name</h2>
-                        <h2 className="p-1">Last Name</h2>
-                        <h2 className="p-1">UserName</h2>
-                        <h2 className="p-1">Email</h2>
-                        <h2 className="p-1">Address</h2>
-                    </section>
-                    {!isEditMode ? (
-                        <section className='flex flex-col md:w-1/2 gap-y-2'>
-                            <h2 className="p-1">{dataUser?.name}</h2>
-                            <h2 className="p-1">{dataUser?.lastName}</h2>
-                            <h2 className="p-1">{dataUser?.userName}</h2>
-                            <h2 className="p-1">{dataUser?.email}</h2>
-                            <h2 className="p-1">{dataUser?.address}</h2>
-                        </section>
-                    ) : (
-                        <form className="flex flex-col gap-2 md:w-1/2  w-1/2 px-5">
-                            <input className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant" type="text" placeholder="Name"/>
-                            <input className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant" type="text" placeholder="Lastname"/>
-                            <input className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant" type="text" placeholder="Username"/>
-                            <input className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant" type="text" placeholder="Email" />
-                            <input className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant" type="text" placeholder="Address"/>
-                        </form>
-                    )}
-                </section>
-                <button
-                onClick={toggleEditMode}
-                className="flex w-40  hover:scale-105 drop-shadow-md justify-center bg-primary text-white font-bold px-3 py-2 rounded-lg mt-4 mb-4 cursor-pointer h-10"
-                >
-                    {isEditMode ? 'Save' : 'Edit Info'}
-                </button>
-            </section>
+          ) : (
+            <form className="flex flex-col gap-2 md:w-1/2  w-1/2 px-5">
+              <input
+                className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant"
+                type="text"
+                placeholder="Name"
+              />
+              <input
+                className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant"
+                type="text"
+                placeholder="Lastname"
+              />
+              <input
+                className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant"
+                type="text"
+                placeholder="Username"
+              />
+              <input
+                className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant"
+                type="text"
+                placeholder="Email"
+              />
+              <input
+                className="md:w-full py-1 px-1 h-8 border rounded shadow-md hover:scale-105 bg-white-variant"
+                type="text"
+                placeholder="Address"
+              />
+            </form>
+          )}
         </section>
-    )
+        <button
+          onClick={toggleEditMode}
+          className="flex w-40  hover:scale-105 drop-shadow-md justify-center bg-primary text-white font-bold px-3 py-2 rounded-lg mt-4 mb-4 cursor-pointer h-10"
+        >
+          {isEditMode ? 'Save' : 'Edit Info'}
+        </button>
+      </section>
+    </section>
+  )
 }
 
 export default Profile
